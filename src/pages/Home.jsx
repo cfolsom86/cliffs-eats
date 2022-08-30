c8600ce2fee47d31d73b6cfcf7fcffbac264171a
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-08-30 11:32:00 -0500

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Veggie from '../components/Veggie'
+import Popular from '../components/Popular'
+import React from 'react'
+function home() {
+  return (
+    <div>
+      <Popular />
+      <Veggie />
+    </div>
+  );
+}
+
+export default home
